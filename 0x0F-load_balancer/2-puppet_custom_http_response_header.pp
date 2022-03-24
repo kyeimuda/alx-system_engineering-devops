01ff0ab5ce081b38fac0879d7fbf8963f86fcf79
kyeimuda <kyeibernard01@gmail.com>
2022-03-23 18:14:45 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,4 @@
 # A puppet script that automates the task of creating an HTTP hearder
-#responds with pippet
 exec {'HTTP_Header':
   command  => 'apt-get -y update && apt-get -y install nginx && echo "Hello World" > /var/www/html/index.nginx-debian.html && CUS_NAME="server_name _;\n\tadd_header X-Served-By $HOSTNAME;" && sed -i "s/server_name _;/$CUS_NAME/" /etc/nginx/sites-available/default && service nginx restart',
   provider => shell,
