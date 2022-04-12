5577dda11a7cc545365737d9f94eb21e91703c3f
kyeimuda <kyeibernard01@gmail.com>
2022-04-11 19:03:28 -0700

    API's, JSON, CSV

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Python script to export data in the JSON format.
+"""
+import csv
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    id_user = int(sys.argv[1])
+
+    req_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+    req_user = requests.get(
+        'https://jsonplaceholder.typicode.com/users').json()
+
+    j_file = sys.argv[1] + '.json'
+
+    with open(j_file, mode='w') as json_file:
+
+        jsonfile = {}
+
+        for i in req_user:
+            if id_user == i.get('id'):
+                USERNAME = i.get('username')
+
+        lists = []
+
+        for j in req_todos:
+            TASK_COMPLETED_STATUS = j.get('completed')
+            TASK_TITLE = j.get('title')
+            if id_user == j.get('userId'):
+                lists.append({'task': TASK_TITLE,
+                              'completed': TASK_COMPLETED_STATUS,
+                              'username': USERNAME})
+                jsonfile = {id_user: lists}
+
+        json.dump(jsonfile, json_file)
