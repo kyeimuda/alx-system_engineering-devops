05265406b8f5959cc1f7e891c02417989d6510e4
kyeimuda <kyeibernard01@gmail.com>
2022-05-05 20:05:38 -0700

    1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message
+exec { 'fix file limit':
+   command => 'useradd -m holberton',
+}
